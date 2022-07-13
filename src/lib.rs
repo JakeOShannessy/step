776c83883727aa7ae28941a9ff4b36990391dddf
Jake O'Shannessy <joshannessy@smokecloud.io>
2022-07-13 10:39:13 +1000

    Parse full data list

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,26 @@
+use std::rc::Rc;
+
 use nom::{
     branch::alt,
-    bytes::complete::{is_not, tag},
+    bytes::complete::{is_not, tag, take_while},
     character::{
-        complete::{alphanumeric1, char, multispace1},
+        complete::{alphanumeric1, char, multispace1, none_of},
         streaming::digit1,
     },
-    combinator::map_res,
+    combinator::{map, map_res, opt},
     multi::separated_list0,
     number::complete::double,
     sequence::{delimited, pair, separated_pair},
     IResult,
 };
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct StepFile {
     pub header: Header,
     pub data: Data,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Header {
     pub file_description: FileDescription,
     pub file_name: FileName,
@@ -49,16 +51,16 @@ pub struct FileName {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct FileSchema {}
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct FilePopulation {}
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct SectionLanguage {}
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct SectionContext {}
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Data {
     pub entities: Vec<Entity>,
 }
@@ -68,11 +70,11 @@ pub struct Data {
 //     pub record:EntityRecord,
 // }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Entity {
     pub id: u64,
     pub name: String,
-    pub s: Vec<String>,
+    pub s: Vec<Parameter>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -92,11 +94,27 @@ pub struct ParameterList(Vec<String>);
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Parameter {
-    SimpleParameter(SimpleParameter),
-    Dollar,
+    TypedParameter(TypedParameter),
+    UntypedParameter(UntypedParameter),
+    Omitted,
+    // SimpleParameter(SimpleParameter),
     Asterisk,
-    // TypedParameter(TypedParameter),
     List(Vec<Parameter>),
+}
+
+// PARAMETER          = TYPED_PARAMETER  |
+//                      UNTYPED_PARAMETER | OMITTED_PARAMETER  .
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum UntypedParameter {
+    SimpleParameter(SimpleParameter),
+    Asterisk,
+    List(Vec<Parameter>),
+}
+#[derive(Debug, Clone, PartialEq)]
+pub struct TypedParameter {
+    pub type_: String,
+    pub value: Rc<Parameter>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -133,7 +151,7 @@ fn ifc_entity_ided(input: &str) -> IResult<&str, Entity> {
     let entity = Entity {
         id,
         name: name.to_string(),
-        s: entity.iter().map(|s| s.to_string()).collect(),
+        s: entity,
     };
     Ok((input, entity))
 }
@@ -151,13 +169,13 @@ fn ifc_occurrence_name(input: &str) -> IResult<&str, Parameter> {
     })(input)?;
     Ok((
         i,
-        Parameter::SimpleParameter(SimpleParameter::OccurrenceName(
-            OccurrenceName::EntityInstanceName(n),
+        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+            SimpleParameter::OccurrenceName(OccurrenceName::EntityInstanceName(n)),
         )),
     ))
 }
 
-fn ifc_entity(input: &str) -> IResult<&str, (&str, Vec<&str>)> {
+fn ifc_entity(input: &str) -> IResult<&str, (&str, Vec<Parameter>)> {
     pair(ifc_name, ifc_parameter_list)(input)
 }
 
@@ -167,24 +185,43 @@ fn ifc_name(input: &str) -> IResult<&str, &str> {
 
 fn ifc_string(input: &str) -> IResult<&str, Parameter> {
     // TODO: allow escaping
-    let (i, s) = delimited(char('\''), is_not("\'"), char('\''))(input)?;
+    let (i, s) = delimited(char('\''), take_while(|c| c != '\''), char('\''))(input)?;
     Ok((
         i,
-        Parameter::SimpleParameter(SimpleParameter::String(s.to_string())),
+        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(SimpleParameter::String(
+            s.to_string(),
+        ))),
     ))
 }
 
-fn ifc_parameter_list(input: &str) -> IResult<&str, Vec<&str>> {
+fn ifc_parameter_list(input: &str) -> IResult<&str, Vec<Parameter>> {
     delimited(char('('), ifc_parameter_list_inner, char(')'))(input)
 }
 
-fn ifc_parameter_list_inner(input: &str) -> IResult<&str, Vec<&str>> {
-    separated_list0(char(','), is_not(",)"))(input)
+fn ifc_typed_parameter(input: &str) -> IResult<&str, TypedParameter> {
+    let (i, (name, value)) = pair(ifc_name, delimited(char('('), ifc_parameter, char(')')))(input)?;
+    Ok((
+        i,
+        TypedParameter {
+            type_: name.to_string(),
+            value: Rc::new(value),
+        },
+    ))
+}
+
+fn ifc_parameter_list_p(input: &str) -> IResult<&str, Parameter> {
+    let (i, list) = ifc_parameter_list(input)?;
+    // TODO: stop cheating with an empty list
+    Ok((i, Parameter::List(vec![])))
+}
+
+fn ifc_parameter_list_inner(input: &str) -> IResult<&str, Vec<Parameter>> {
+    separated_list0(char(','), ifc_parameter)(input)
 }
 
 fn ifc_dollar(input: &str) -> IResult<&str, Parameter> {
     let (i, _) = char('$')(input)?;
-    Ok((i, Parameter::Dollar))
+    Ok((i, Parameter::Omitted))
 }
 
 fn ifc_asterisk(input: &str) -> IResult<&str, Parameter> {
@@ -196,13 +233,18 @@ fn ifc_enum(input: &str) -> IResult<&str, Parameter> {
     let (i, enum_value) = delimited(char('.'), is_not("."), char('.'))(input)?;
     Ok((
         i,
-        Parameter::SimpleParameter(SimpleParameter::EnumerationValue(enum_value.to_string())),
+        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+            SimpleParameter::EnumerationValue(enum_value.to_string()),
+        )),
     ))
 }
 
 fn ifc_num(input: &str) -> IResult<&str, Parameter> {
     let (i, n) = double(input)?;
-    Ok((i, Parameter::SimpleParameter(SimpleParameter::Real(n))))
+    Ok((
+        i,
+        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(SimpleParameter::Real(n))),
+    ))
 }
 
 fn ifc_parameter(input: &str) -> IResult<&str, Parameter> {
@@ -213,8 +255,9 @@ fn ifc_parameter(input: &str) -> IResult<&str, Parameter> {
         ifc_enum,
         ifc_num,
         ifc_occurrence_name,
+        ifc_parameter_list_p,
+        map(ifc_typed_parameter, Parameter::TypedParameter),
     ))(input)
-    // separated_list0(char(','), is_not(",)"))(input)
 }
 
 #[cfg(test)]
@@ -243,18 +286,20 @@ mod tests {
                      id: 1,
                      name: "IFCORGANIZATION".to_string(),
                      s: vec![
-                        "$","'Autodesk Revit Architecture 2011'","$","$","$"
-                    ].iter().map(|s|s.to_string()).collect(),
+                        Parameter::Omitted, Parameter::UntypedParameter(UntypedParameter::SimpleParameter(SimpleParameter::String("Autodesk Revit Architecture 2011".to_string()))), Parameter::Omitted, Parameter::Omitted, Parameter::Omitted
+                    ],
                 }
                 ,Entity{
                     id:2,
                     name:"IFCAPPLICATION".to_string(),
                     s:vec![
-                        "#1",
-                        "'2011'",
-                        "'Autodesk Revit Architecture 2011'",
-                        "'Revit'"
-                    ].iter().map(|s|s.to_string()).collect()}
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(SimpleParameter::OccurrenceName(
+                        OccurrenceName::EntityInstanceName(1)
+                    ))),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(SimpleParameter::String("2011".to_string()))),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(SimpleParameter::String("Autodesk Revit Architecture 2011".to_string()))),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(SimpleParameter::String("Revit".to_string()))),
+                    ]}
     ]
             ))
         );
@@ -272,14 +317,19 @@ mod tests {
                     id: 2,
                     name: "IFCAPPLICATION".to_string(),
                     s: vec![
-                        "#1",
-                        "'2011'",
-                        "'Autodesk Revit Architecture 2011'",
-                        "'Revit'"
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::OccurrenceName(OccurrenceName::EntityInstanceName(1))
+                        )),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::String("2011".to_string())
+                        )),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::String("Autodesk Revit Architecture 2011".to_string())
+                        )),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::String("Revit".to_string())
+                        ))
                     ]
-                    .iter()
-                    .map(|s| s.to_string())
-                    .collect()
                 }
             ))
         );
@@ -297,14 +347,19 @@ mod tests {
                     id: 2,
                     name: "IFCAPPLICATION".to_string(),
                     s: vec![
-                        "#1",
-                        "'2011'",
-                        "'Autodesk Revit Architecture 2011'",
-                        "'Revit'"
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::OccurrenceName(OccurrenceName::EntityInstanceName(1))
+                        )),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::String("2011".to_string())
+                        )),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::String("Autodesk Revit Architecture 2011".to_string())
+                        )),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::String("Revit".to_string())
+                        ))
                     ]
-                    .iter()
-                    .map(|s| s.to_string())
-                    .collect()
                 }
             ))
         );
@@ -319,10 +374,18 @@ mod tests {
                 (
                     "IFCAPPLICATION",
                     vec![
-                        "#1",
-                        "'2011'",
-                        "'Autodesk Revit Architecture 2011'",
-                        "'Revit'"
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::OccurrenceName(OccurrenceName::EntityInstanceName(1))
+                        )),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::String("2011".to_string())
+                        )),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::String("Autodesk Revit Architecture 2011".to_string())
+                        )),
+                        Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::String("Revit".to_string())
+                        ))
                     ]
                 )
             ))
@@ -335,8 +398,8 @@ mod tests {
             ifc_string("'Autodesk Revit Architecture 2011'"),
             Ok((
                 "",
-                Parameter::SimpleParameter(SimpleParameter::String(
-                    "Autodesk Revit Architecture 2011".to_string()
+                Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                    SimpleParameter::String("Autodesk Revit Architecture 2011".to_string())
                 ))
             ))
         );
@@ -344,16 +407,88 @@ mod tests {
 
     #[test]
     fn parse_ifc_parameter_list() {
-        assert_eq!(ifc_parameter_list_inner("a,b"), Ok(("", vec!["a", "b",])));
+        assert_eq!(
+            ifc_parameter_list_inner("'a','b'"),
+            Ok((
+                "",
+                vec![
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::String("a".to_string())
+                    )),
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::String("b".to_string())
+                    )),
+                ]
+            ))
+        );
         assert_eq!(
             ifc_parameter_list_inner("#1,'2011','Autodesk Revit Architecture 2011','Revit'"),
             Ok((
                 "",
                 vec![
-                    "#1",
-                    "'2011'",
-                    "'Autodesk Revit Architecture 2011'",
-                    "'Revit'"
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::OccurrenceName(OccurrenceName::EntityInstanceName(1))
+                    )),
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::String("2011".to_string())
+                    )),
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::String("Autodesk Revit Architecture 2011".to_string())
+                    )),
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::String("Revit".to_string())
+                    ))
+                ]
+            ))
+        );
+    }
+
+    #[test]
+    fn parse_ifc_parameter_list2() {
+        assert_eq!(
+            ifc_string("''"),
+            Ok((
+                "",
+                Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                    SimpleParameter::String("".to_string())
+                ))
+            ))
+        );
+        assert_eq!(
+            ifc_parameter_list_inner("$,'','',$,$"),
+            Ok((
+                "",
+                vec![
+                    Parameter::Omitted,
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::String("".to_string())
+                    )),
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::String("".to_string())
+                    )),
+                    Parameter::Omitted,
+                    Parameter::Omitted,
+                ]
+            ))
+        );
+        assert_eq!(
+            ifc_parameter_list_inner(
+                "'Reference',$,IFCLABEL('Basic Wall:Interior - Partition (92mm Stud)'),$"
+            ),
+            Ok((
+                "",
+                vec![
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::String("Reference".to_string())
+                    )),
+                    Parameter::Omitted,
+                    Parameter::TypedParameter(TypedParameter {
+                        type_: "IFCLABEL".to_string(),
+                        value: Rc::new(Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                            SimpleParameter::String("Basic Wall:Interior - Partition (92mm Stud)".to_string())
+                        ))),
+                    }),
+                    Parameter::Omitted,
                 ]
             ))
         );
@@ -366,10 +501,18 @@ mod tests {
             Ok((
                 "",
                 vec![
-                    "#1",
-                    "'2011'",
-                    "'Autodesk Revit Architecture 2011'",
-                    "'Revit'"
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::OccurrenceName(OccurrenceName::EntityInstanceName(1))
+                    )),
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::String("2011".to_string())
+                    )),
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::String("Autodesk Revit Architecture 2011".to_string())
+                    )),
+                    Parameter::UntypedParameter(UntypedParameter::SimpleParameter(
+                        SimpleParameter::String("Revit".to_string())
+                    )),
                 ]
             ))
         );
